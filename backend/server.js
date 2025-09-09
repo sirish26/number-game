ffda802993bd39a31bda2235da4dae79623c80e3
vmsirish <sirishvm@gmail.com>
2025-09-09 08:48:15 +0530

    added play again functionality

--- backend/server.js
+++ backend/server.js
@@ -16,16 +16,22 @@ io.on("connection", (socket) => {
   console.log("Player connected:", socket.id);
 
   players[socket.id] = { choice: null };
-
+  
   if (Object.keys(players).length === 2) {
     io.emit("startGame", { message: "Game started" });
+    console.log("Game started between:", Object.keys(players));
+  } else {
+    socket.emit("waiting", { message: "Waiting for another player..." });
   }
 
   socket.on("chooseNumber", (num) => {
+    if (!players[socket.id]) return;
+
     players[socket.id].choice = num;
+    console.log(`Player ${socket.id} chose number:`, num);
 
     const choices = Object.values(players).map((p) => p.choice);
-    if (choices.every((c) => c !== null)) {
+    if (choices.every((c) => c !== null) && Object.keys(players).length === 2) {
       const [p1, p2] = Object.keys(players);
       const [c1, c2] = choices;
 
@@ -44,14 +50,28 @@ io.on("connection", (socket) => {
       });
 
       randomNumber = Math.floor(Math.random() * 10) + 1;
-      players = {};
+      Object.keys(players).forEach((id) => {
+        if (players[id]) players[id].choice = null;
+      });
     }
+    console.log("Random number for this round:", randomNumber);
+  });
+
+  socket.on("playAgain", () => {
+    console.log("Play Again clicked by", socket.id);
+    Object.keys(players).forEach((id) => {
+      if (players[id]) players[id].choice = null;
+    });
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    io.emit("startGame", { message: "Game restarted! Pick your number." });
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
     console.log("Player disconnected:", socket.id);
+
+    io.emit("waiting", { message: "Player left. Waiting for next person..." });
   });
 });
 
-server.listen(3001, () => console.log("Backend running"));
+server.listen(3001, () => console.log("Backend running on port 3001"));
